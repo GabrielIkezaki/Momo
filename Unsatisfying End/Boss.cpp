1e7e82002404fc167050cce77b8431104f209f31
Gabriel Ikezaki <gabriel.mori@live.com>
2021-05-10 21:25:57 -0300

    Added movement to hanako, red blink effect when damaged

--- Unsatisfying End/Boss.cpp
+++ Unsatisfying End/Boss.cpp
@@ -1,9 +1,40 @@
 #include "Boss.h"
 
 Boss::Boss()
-	:Character(5000, sf::Vector2f(220, 10), "./Sprites/Characters/hanako.png", sf::Vector2f(.7f, .7f), sf::Vector2f(190, 250), sf::Vector2f(55,60)) {}
+	:Character(5000, sf::Vector2f(180, 10), "./Sprites/Characters/hanako.png", sf::Vector2f(.7f, .7f), sf::Vector2f(70, 70), sf::Vector2f(170,130)) {}
+
+void Boss::ArcMovement() {
+	timer = clock.getElapsedTime();
+
+	Character::Move(sf::Vector2f(ArcXVelocity(), ArcYVelocity()));
+	
+}
+
+float Boss::ArcYVelocity() {
+	float yVelocity = 100 * std::cos(2 * timer.asSeconds());
+	return yVelocity;
+}
+
+
+float Boss::ArcXVelocity() {
+	float yVelocity = 100 * std::cos(timer.asSeconds()/3);
+	return yVelocity;
+}
+
+void Boss::TakeDamage(int tempDamage) {
+	Character::TakeDamage(tempDamage);
+
+	blinkClock.restart();
+	characterSprite.objectSprite.setColor(blinkColor);
+}
 
 void Boss::Update() {
+	ArcMovement();
 	UpdateRect();
+	blinkTimer = blinkClock.getElapsedTime();
+
+	if (blinkTimer.asSeconds() > 0.05f) {
+		characterSprite.objectSprite.setColor(sf::Color::White);
+	}
 }
 
--- Unsatisfying End/Boss.h
+++ Unsatisfying End/Boss.h
@@ -1,10 +1,23 @@
 #pragma once
 #include "Character.h"
+
 class Boss :
     public Character
 {
+
+private:
+    sf::Clock clock, blinkClock;
+    sf::Time timer, blinkTimer;
+    sf::Color blinkColor = sf::Color(255,0,0,230);
+    float xVelocity;
 public:
     Boss();
     void Update();
+    void ArcMovement();
+
+    void TakeDamage(int tempDamage)override;
+
+    float ArcYVelocity();
+    float ArcXVelocity();
 };
 
